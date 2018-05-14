27f0004845a662b5ec0881630c8f311ff4383442
sunaliseth <sunaliseth01@gmail.com>
2018-05-14 18:56:37 +0100

    Shallow test for button in the App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, mount, render } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
@@ -7,3 +8,9 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('has a "press me" button', () => {
+  const wrapper = shallow(<App />)
+  const text = wrapper.find('button').text()
+  expect(text).toEqual('USE THE FORCE press me!')
+})
