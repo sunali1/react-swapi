650bdfece5b09e4acc401931e00ae890b7957a17
sunaliseth <sunaliseth01@gmail.com>
2018-05-14 11:54:20 +0100

    Fetch data function to get the data from the Star Wars API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const URL = 'https://swapi.co/api/';
+const QUERY = 'people/'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { people: [] };
+    //this._getData = this._getData.bind(this);
+  }
+  getData() {
+    fetch(URL + QUERY)
+    .then(response => response.json())
+    .then(people => this.setState({ people: people.results }))
+    .catch(error => console.log(`Disturbance in the force there is: ${error}`))
+  }
+
   render() {
+    console.log(this.getData(),
+      `2 render this.state.people ${this.state.people}`)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">REACT Star Wars API</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
         </p>
       </div>
     );
